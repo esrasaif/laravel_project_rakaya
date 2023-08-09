7b63b8006ea3c9682a7c45e676781875d3e9a3b2
esra <esro.155@gmail.com>
2023-08-09 05:55:54 +0300

    create blade components to style the pages (does not customize it yet)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,39 +1,35 @@
 <x-layout>
+ 
+  @include('posts-header')
 
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+  
 
-{{-- here posts variable store array of all posts and with its metadata so we can displat the metadata and also the body --}}
-  <?php  foreach($posts as $post):  ?>
+  @if( $posts->count())
 
+  <x-post-featured-card :post="$posts[0]" />
 
-    <article>
-       
-        <h1> 
-          
-          <h4>title</h4>
-          wrote by: <a href="authors/{{$post->author->username}}">{{$post->author->username}}  </a>
-          <br>
-
-          <a href="/posts/<?=  $post->id ;   ?> ">
-          {!!  $post->title !!} 
-          </a>
-        
-        
-        </h1>  
-
-        <div>
-
-          <h4>category</h4>
-          <a href="/categories/{{$post->category->id}}">{{$post->category->name}}</a>
-
+  @if( $posts->count() >1)
+        <div class="lg:grid lg:grid-cols-2">
+          @foreach( $posts->skip(1) as $post)
+        <x-post-card :post="$post" class="bg-red-100" />
+        @endforeach
         </div>
+  @endif
+
+ @else
+ <p class="text-center"> NO ,Posts  </p>
+ @endif
 
 
 
+{{-- 
+    <div class="lg:grid lg:grid-cols-3">
+      <x-post-card />
+      <x-post-card />
+    </div> --}}
 
-    </article>
-  
 
-  
-  <?php endforeach; ?>
+</main>
 
 </x-layout>
