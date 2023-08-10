226b41de215f9eaa3f63470b0ad8067507c52a82
esra <esro.155@gmail.com>
2023-08-10 14:44:45 +0300

    get result of both queries of search and category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -30,9 +30,10 @@ class Post extends Model
         // search filter
         $query->when( $filters['search'] ?? false , function($query,$search) 
         {
-            $query
-            ->where( 'title','like','%'.$search.'%' )
-            ->orWhere( 'body','like','%'.$search.'%' );
+            $query->where(fn( $query) =>
+                $query->where( 'title','like','%'.$search.'%' )
+                ->orWhere( 'body','like','%'.$search.'%' )
+                );
         });
        
 
